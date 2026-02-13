2d23428a36583baa86594b64fea490093798aeb4
Ahmed <Ahmed@AHMED-PC>
2026-02-13 13:19:37 +0300

    Add Syncfusion refs and new forms (Form2, frmMain)
    Added Syncfusion 32.1.19 library references and packages.config. Introduced Form2 (empty) and frmMain (with Syncfusion controls). Updated DVLD.csproj to include new forms and resources.

---
+++ DVLD/frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DVLD
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.colorPickerButton1 = new Syncfusion.Windows.Forms.ColorPickerButton();
+            this.bannerTextProvider1 = new Syncfusion.Windows.Forms.BannerTextProvider(this.components);
+            this.autoLabel1 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            this.autoComplete1 = new Syncfusion.Windows.Forms.Tools.AutoComplete(this.components);
+            this.popupMenu1 = new Syncfusion.Windows.Forms.Tools.XPMenus.PopupMenu(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.autoComplete1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorPickerButton1
+            // 
+            this.colorPickerButton1.AccessibilityEnabled = true;
+            this.colorPickerButton1.BeforeTouchSize = new System.Drawing.Size(250, 130);
+            this.colorPickerButton1.Location = new System.Drawing.Point(329, 128);
+            this.colorPickerButton1.Name = "colorPickerButton1";
+            this.colorPickerButton1.Size = new System.Drawing.Size(250, 130);
+            this.colorPickerButton1.TabIndex = 0;
+            this.colorPickerButton1.Text = "colorPickerButton1";
+            // 
+            // autoLabel1
+            // 
+            this.autoLabel1.Location = new System.Drawing.Point(112, 163);
+            this.autoLabel1.Name = "autoLabel1";
+            this.autoLabel1.Size = new System.Drawing.Size(60, 13);
+            this.autoLabel1.TabIndex = 1;
+            this.autoLabel1.Text = "autoLabel1";
+            // 
+            // autoComplete1
+            // 
+            this.autoComplete1.HeaderFont = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.autoComplete1.ItemFont = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.autoComplete1.MetroColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
+            this.autoComplete1.ParentForm = this;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.autoLabel1);
+            this.Controls.Add(this.colorPickerButton1);
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            ((System.ComponentModel.ISupportInitialize)(this.autoComplete1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.ColorPickerButton colorPickerButton1;
+        private Syncfusion.Windows.Forms.BannerTextProvider bannerTextProvider1;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel1;
+        private Syncfusion.Windows.Forms.Tools.AutoComplete autoComplete1;
+        private Syncfusion.Windows.Forms.Tools.XPMenus.PopupMenu popupMenu1;
+    }
+}
